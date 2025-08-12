9382c8b769e02efdc64056a6dd5687d3adbd71bf
NataliaG1711 <nataliagiraldoamador@gmail.com>
2025-08-12 00:10:41 -0500

    user logo

--- web_project1/web_project/src/components/Encabezado.jsx
+++ web_project1/web_project/src/components/Encabezado.jsx
@@ -1,14 +1,20 @@
 import "../styles/Encabezado.css"
+import logo from "../images/logo.png"
+import user from "../images/usuario.png"
 
 function Encabezado() {
     return (
         <>
             <header className="encabezado">
                 <nav className="nav">
-                    <ul className="nav-list">
-                        <li> <a href="#">Home</a></li>
-                        <li> <a href="#">Movies</a></li>
-                    </ul>
+                    <img className="logo" src={logo} alt="Logo" />
+                    <div className="nav-center">
+                        <ul className="nav-list">
+                            <li> <a href="#">Home</a></li>
+                            <li> <a href="#">Movies</a></li>
+                        </ul>
+                    </div>
+                    <img className="user-logo" src={user} alt="User Logo" />
                 </nav>
             </header>
         </>
